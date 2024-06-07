cbafa7a88067f40182515090a597186d12d956ee
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-06-07 02:09:33 +0200

    chore: Add email and password validation to registration form

--- angular/src/app/auth/register/register.component.ts
+++ angular/src/app/auth/register/register.component.ts
@@ -28,15 +28,30 @@ export class RegisterComponent {
       username: this.fb.control(null, [Validators.required]),
       email: this.fb.control(null, [Validators.required]),
       password: this.fb.control(null, [Validators.required]),
+      confirmPassword: this.fb.control(null, [Validators.required]),
     });
   }
 
-  isTouchedInvalid(fieldName: string) {
+  // Metodo per la validazione dell'email
+  validateEmail(email: string): boolean {
+    const emailPattern: RegExp =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  // Metodo per la validazione della password
+  validatePassword(password: string): boolean {
+    const passwordPattern: RegExp =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordPattern.test(password);
+  }
+
+  isTouchedInvalid(fieldName: string): boolean | undefined {
     const control = this.form.get(fieldName);
     return control?.touched && control?.invalid;
   }
 
-  register() {
+  register(): void {
     this.authSvc.register(this.form.value).subscribe((data) => {
       alert('User registered successfully!');
       this.router.navigate(['/auth/login']);
@@ -44,8 +59,30 @@ export class RegisterComponent {
     });
   }
 
-  onSubmit() {
-    console.log(this.form.value);
-    console.log(this.user);
+  onSubmit(): void {
+    if (this.form.valid) {
+      const { email, password, confirmPassword } = this.form.value;
+      const isEmailValid: boolean = this.validateEmail(email);
+      const isPasswordValid: boolean = this.validatePassword(password);
+      const passwordsMatch: boolean = password === confirmPassword;
+
+      if (isEmailValid && isPasswordValid && passwordsMatch) {
+        this.register();
+      } else {
+        let errorMessage = '';
+        if (!isEmailValid) {
+          errorMessage += "L'email inserita non è valida. ";
+        }
+        if (!isPasswordValid) {
+          errorMessage +=
+            'La password deve contenere almeno una lettera maiuscola, una minuscola, un numero e un carattere speciale, e deve essere lunga almeno 8 caratteri. ';
+        }
+        if (!passwordsMatch) {
+          errorMessage +=
+            'Le password non corrispondono. Per favore, inserisci la stessa password in entrambi i campi.';
+        }
+        console.log(errorMessage);
+      }
+    }
   }
 }
