7a669ed020b2a9b29d20e49f2b3a6dcfd294717e
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-06-07 15:03:15 +0200

    Refactor profile component to remove unused CardComponent import and update film card layout

--- angular/src/app/pages/profile/profile.module.ts
+++ angular/src/app/pages/profile/profile.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
-import { CardComponent } from '../../shared-components/card/card.component';
 
 @NgModule({
   declarations: [ProfileComponent],
