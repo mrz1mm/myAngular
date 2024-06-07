e7ed630ca1d5b5aec3281c28cc800a63a39adce4
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-06-07 17:16:12 +0200

    Refactor user component to display user data and update user image URL

--- angular/src/app/pages/home/home.module.ts
+++ angular/src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared-components/shared.module';
+import { UserComponent } from '../../shared-components/user/user.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, UserComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
--- angular/src/app/shared-components/user/user.component.ts
+++ angular/src/app/shared-components/user/user.component.ts
@@ -1,8 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { iUser } from '../../auth/interfaces/i-user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {}
+export class UserComponent {
+  private authSvc = inject(AuthService);
+  @Input() userCard!: iUser;
+}
